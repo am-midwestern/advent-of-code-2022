f89b14610c3f2ba58dc36d89948403167758b063
Aaron Merrick <am@buildmidwestern.com>
2022-12-22 10:08:17 -0600

    feat: part 2 solution

--- Day 8/main.js
+++ Day 8/main.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const readline = require('readline');
 
 var user_file = './Day 8/input_text.txt';
+var test_file = './Day 8/test.txt';
 let result = '';
 
 function main () {
@@ -18,28 +19,68 @@ function main () {
     r.on('close', () => {
         let permimeter = (grid[0].length * 2) + (grid.length * 2) - 4;
         let interior = 0;
+        let scores = [];
 
-        // Puzzle 1
         for (let r = 1; r < grid.length - 1; r++) {
             for (let c = 1; c < grid[r].length - 1; c++) {
-                currPosition = grid[r][c];
-                if (
-                    // check left or right
-                    grid[r].slice(0,c).every(v => v < currPosition) ||
-                    grid[r].slice(c+1).every(v => v < currPosition) ||
-                    // check up and down
-                    grid.map(row => row[c]).filter((row, index) => index < r).every(v => v < currPosition) ||
-                    grid.map(row => row[c]).filter((row, index) => index > r).every(v => v < currPosition)
-                ) {
-                    interior++;
+                let currPosition = grid[r][c];
+
+                let left = grid[r].slice(0,c);
+                let right = grid[r].slice(c+1);
+                let up = grid.map(row => row[c]).filter((row, index) => index < r);
+                let down = grid.map(row => row[c]).filter((row, index) => index > r);
+
+                // Puzzle 1
+                // if (
+                //     // check left or right
+                //     left.every(v => v < currPosition) ||
+                //     right.every(v => v < currPosition) ||
+                //     // check up and down
+                //     up.every(v => v < currPosition) ||
+                //     down.every(v => v < currPosition)
+                // ) {
+                //     interior++;
+                // }
+
+                // result = permimeter + interior;
+
+                // Puzzle 2
+                let blockers = {};
+
+                let blockL = left.reverse().findIndex(v => v >= currPosition);
+                let blockR = right.findIndex(v => v >= currPosition);
+                let blockU = up.reverse().findIndex(v => v >= currPosition);
+                let blockD = down.findIndex(v => v >= currPosition);
+
+                if (blockL === -1) {
+                    blockers['left'] = left.length;
+                } else {
+                    blockers['left'] = blockL + 1;
                 }
 
+                if (blockR === -1) {
+                    blockers['right'] = right.length;
+                } else {
+                    blockers['right'] = (c + blockR + 1) - c;
+                }
+
+                if (blockU === -1) {
+                    blockers['up'] = up.length;
+                } else {
+                    blockers['up'] = blockU + 1;
+                }
+
+                if (blockD === -1) {
+                    blockers['down'] = down.length;
+                } else {
+                    blockers['down'] = (r + blockD + 1) - r;
+                }
+
+                scores.push(Object.values(blockers).reduce((a,b) => a * b, 1));
             }
         }
 
-        // Puzzle 2
-
-        result = permimeter + interior;
+        result = Math.max(...scores);
         console.log(result);
     });
 };
